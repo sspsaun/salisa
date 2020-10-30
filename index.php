a97088bc26f624a15720830bdfc39c96415b4f39
sspsaun <73693969+sspsaun@users.noreply.github.com>
2020-10-30 17:11:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Hello world</title>
+  </head>
+
+  <body>
+    <h1>My First Website</h1>
+    <img
+      src="hong.jpg"
+    />
+    <br />
+    <a href="https://www.facebook.com/salisa.phosit.5/">My contact</a>
+
+    <ul>
+      <li>My name is Salisa Phosit.</li>
+      <li>You can called me Aun.</li>
+    </ul>
+    <h3>Joke</h3>
+    <img
+      src="555.jpg"
+    />
+    <br />
+    <p>เมื่อวานครับ</p>
+    <p>ทำไมครับ</p>
+    <p>ผมไปหาคุณหมอที่คลินิกมาครับ</p>
+    <p>แล้วไงต่อครับ</p>
+    <p>หมอบอกผมว่า คุณเหลือเวลาอีกไม่นานแล้ว</p>
+    <p>ทำไมครับ คุณเป็นอะไรหรอ</p>
+    <p>หมอบอก คลินิกปิด2ทุ่ม</p>
+    <p>. . .</p>
+  </body>
+</html>
