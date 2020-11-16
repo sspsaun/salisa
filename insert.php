348b0bb14fbef7f47d98dd5634b45d2957682f58
sspsaun <73693969+sspsaun@users.noreply.github.com>
2020-11-16 22:38:42 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,17 +7,17 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$id = $_GET['ID'];
+
 $name = $_POST['name'];
 $comment = $_POST['comment'];
-$link = $_POST['link'];
 
 
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+$sql = "INSERT INTO guestbook (Name , Comment) VALUES ('$name', '$comment')";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+     echo "<script>alert('New record created successfully')</script>";
+      echo "<script>window.location='show.php'</script>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
