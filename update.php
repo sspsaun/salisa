1b20b446652990441402a5a3bdc0a517fb9c6ce2
sspsaun <73693969+sspsaun@users.noreply.github.com>
2020-11-27 17:49:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -6,7 +6,7 @@ $name=$_POST['name'];
 $weight=$_POST['weight'];
 $height=$_POST['height'];
 $bmi = $weight / (($height/100)**2);
-$sql="UPDATE itf SET name='$name', weight='$weight', height='$height', bmi='bmi' WHERE ID='$id'";
+$sql="UPDATE itf SET weight='$weight', height='$height', bmi='$bmi' WHERE ID='$id'";
 if (mysqli_query($conn, $sql)) {
     header("Location: show.php");
   } else {
