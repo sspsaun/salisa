e9cc8c9c42777a6d53c14c17d2ec328965a39993
sspsaun <73693969+sspsaun@users.noreply.github.com>
2020-11-27 17:36:12 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,44 +1,30 @@
-<?php
-include("connect.php");
-    $id=$_GET['id'];
-    $infosql = "SELECT * FROM itf WHERE ID = '".$id."'";
-    $inforesult = mysqli_query($conn,$infosql);
-    $res = mysqli_fetch_array($inforesult);
-    $weight = $_POST['weight'];
-    $height = $_POST['height'];
- if(isset($_POST['submit'])){
-    
-    $update = "UPDATE itf SET weight = '".$weight."', height = '".$height."' WHERE itf.`ID` = '".$id."';";  
-    $result= mysqli_query($conn,$update);
-    if($result){
-        echo "<script>alert('แก้ไขเสร็จสิ้น')</script>";
-        echo "<script>window.location='show.php'</script>";
-
-    }else{
-        echo "<script>alert('error')</script>";
-        echo "<script>window.location='show.php'</script>";
-
-    }
- }
-
-?>
+<!DOCTYPE html>
 <html>
-    <head>
-        <style>
-            textarea{
-                resize:none;
-            }
-        </style>
-    </head>
+<head>
+  <title>Edit Form</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
     <body>
-    <form method = "POST" id="CommentForm" >
-    Weight:<br>
-    <input type="text" name = "weight" value="<?php echo $res['weight'];?>"> <br>
-    Height:<br>
-    <input type="text" name="height" value="<?php echo $res['height'];?>"><br>
-    
-    <input type="submit" name="submit">
-  </form> 
-    </body>
-    
-</html>
+    <?php
+    $id=$_GET['ID'];
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'salisa11.mysql.database.azure.com', 'salisa11@salisa11', 'Goatmilk11', 'itflab', 3306);
+    $res = mysqli_query($conn, "SELECT * FROM itf WHERE ID='$id'");
+    $row = mysqli_fetch_array($res)
+    ?>
+    <div class="container">
+    <h2>Edit Form</h2>
+    <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
+        <label for="Username">Price :</label>
+          <input type="text" class="form-control" name = "weight" id="idweight" value="<?php echo "$row[weight]"; ?>" <br>
+        <label for="Username">Discount :</label>
+          <input type="text" class="form-control" name = "height" id="height" value="<?php echo "$row[height]"; ?>"> <br><br>
+          <input type="submit" id="commentBtn"class="btn btn-outline-warning">
+    </div>
+      </form>
+</body>
